87c2c3e779b731c847761c437ad081ed4cb299c2
Fanny Gautier <fannygautier92@gmail.com>
2022-11-29 16:14:27 +0100

    Creation migration table + model

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/user',function (){
+
+});
+
+Route::post('/user',function (){
+
+});
+
+Route::delete('/user',function (){
+
+});
+
+
+Route::put('/user',function (){
+
+});
