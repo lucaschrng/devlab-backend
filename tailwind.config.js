8394f271025e5c6d1c2a2ee8bfea961a56714b4c
Lucas Charoing <charoing.lucas@gmail.com>
2022-11-30 01:28:18 +0100

    Add interactive search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'bg': '#1E1E1E',
         'lighter-bg': '#383838',
       },
       boxShadow: {
         'small': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      },
+      gridColumn: {
+        'overlap': '1 / span 1',
+      },
+      gridRow: {
+        'overlap': '1 / span 1',
       },
     },
   },
