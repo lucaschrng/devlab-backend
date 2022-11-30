778f59d2b68fc5eb1a8f648486caca11f8f55040
Lucas Charoing <charoing.lucas@gmail.com>
2022-11-30 16:37:52 +0100

    Add homepage best movies of all time section

--- public/js/app.js
+++ public/js/app.js
@@ -14,14 +14,17 @@ var searchBtn = document.querySelector('.search-label');
 var closeBtn = document.querySelector('.close-label');
 var resultsDiv = document.querySelector('.results');
 var resultsSection = document.querySelector('.results-section');
+var query = document.querySelector('.query');
+var sort = document.querySelector('.select-sort');
 var main = document.querySelector('main');
 var keywords = encodeURI(searchInput.value);
+var bestMovies = document.querySelector('.best-movies');
+var bestPrevious = document.querySelector('.best-movies-container > .previous-button');
+var bestNext = document.querySelector('.best-movies-container > .next-button');
+var translate = 0;
 searchInput.addEventListener('keyup', function () {
   keywords = encodeURI(searchInput.value);
-  elementsNb = resultsDiv.childElementCount;
-  for (var i = 0; i < elementsNb; i++) {
-    resultsDiv.removeChild(resultsDiv.firstChild);
-  }
+  query.innerHTML = searchInput.value;
   if (keywords !== '') {
     searchMovies(keywords, 1);
   }
@@ -35,24 +38,83 @@ closeBtn.addEventListener('click', function () {
   resultsSection.classList.add('hidden');
   main.classList.remove('hidden');
 });
+sort.addEventListener('change', function () {
+  keywords = encodeURI(searchInput.value);
+  query.innerHTML = searchInput.value;
+  if (keywords !== '') {
+    searchMovies(keywords, 1);
+  }
+});
+if (bestMovies !== null) {
+  searchBest();
+  bestNext.addEventListener('click', function () {
+    if (translate < 19) {
+      translate++;
+    }
+    console.log(document.querySelectorAll('.best-movies > *'));
+    document.querySelectorAll('.best-movies > *').forEach(function (movieCard) {
+      movieCard.style.translate = 'calc(' + -translate * 100 + '% + ' + -translate * 1.5 + 'rem)';
+    });
+  });
+  bestPrevious.addEventListener('click', function () {
+    if (translate > 0) {
+      translate--;
+    }
+    console.log(document.querySelectorAll('.best-movies > *'));
+    document.querySelectorAll('.best-movies > *').forEach(function (movieCard) {
+      movieCard.style.translate = 'calc(' + -translate * 100 + '% + ' + -translate * 1.5 + 'rem)';
+    });
+  });
+}
 function searchMovies(keywords, page) {
   axios.get('https://api.themoviedb.org/3/search/movie?api_key=b0c77f111b96a7cafe54d722516ddeff&language=en-US&query=' + keywords + '&page= ' + page + '&include_adult=false').then(function (response) {
+    elementsNb = resultsDiv.childElementCount;
+    for (var i = 0; i < elementsNb; i++) {
+      resultsDiv.removeChild(resultsDiv.firstChild);
+    }
     console.log(response.data.results);
     displayResults(response.data.results);
   })["catch"](function (error) {
     console.log(error);
   });
 }
+function searchBest() {
+  axios.get('https://api.themoviedb.org/3/discover/movie?api_key=b0c77f111b96a7cafe54d722516ddeff&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=10000&with_watch_monetization_types=flatrate').then(function (response) {
+    elementsNb = bestMovies.childElementCount;
+    for (var i = 0; i < elementsNb; i++) {
+      bestMovies.removeChild(bestMovies.firstChild);
+    }
+    console.log(response.data.results);
+    response.data.results.forEach(function (movie) {
+      displayMovie(movie, bestMovies);
+    });
+  })["catch"](function (error) {
+    console.log(error);
+  });
+}
 function displayResults(movies) {
+  if (sort.value === 'name') {
+    movies.sort(function (a, b) {
+      return a.title.localeCompare(b.title);
+    });
+  } else if (sort.value === 'popularity') {
+    movies.sort(function (a, b) {
+      return b.popularity - a.popularity;
+    });
+  } else if (sort.value === 'rating') {
+    movies.sort(function (a, b) {
+      return b.vote_average - a.vote_average;
+    });
+  }
   movies.forEach(function (movie) {
-    displayMovie(movie);
+    displayMovie(movie, resultsDiv);
   });
   addEmptyDivs();
 }
-function displayMovie(movie) {
+function displayMovie(movie, container) {
   if (typeof movie.poster_path === 'string') {
     var movieCard = document.createElement('div');
-    movieCard.classList.add('max-w-[250px]', 'mt-6');
+    movieCard.classList.add('min-w-[250px]', 'mt-10', 'transition-all', 'duration-300');
     var posterContainer = document.createElement('a');
     posterContainer.href = '/movie/' + movie.id;
     var poster = document.createElement('img');
@@ -76,7 +138,7 @@ function displayMovie(movie) {
     posterContainer.appendChild(poster);
     movieCard.appendChild(posterContainer);
     movieCard.appendChild(infos);
-    resultsDiv.appendChild(movieCard);
+    container.appendChild(movieCard);
   }
 }
 function addEmptyDivs() {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,10 +12,16 @@
 </head>
 
 <body>
-    <header>
+    <header class="sticky top-0 z-10">
         <x-navbar />
     </header>
-    <main class="px-24">
+    <main class="py-24">
+        <h2 class="px-24 text-3xl font-medium">Best movies of all time</h2>
+        <div class="best-movies-container grid items-center">
+            <ion-icon name="chevron-back-outline" class="previous-button ml-6 p-3 text-3xl rounded-full col-overlap row-overlap cursor-pointer bg-black/70 backdrop-blur z-10"></ion-icon>
+            <ion-icon name="chevron-forward-outline" class="next-button mr-6 p-3 text-3xl rounded-full col-overlap row-overlap cursor-pointer bg-black/70 backdrop-blur justify-self-end z-10"></ion-icon>
+            <div class="best-movies px-24 flex max-w-full overflow-hidden gap-6 col-overlap row-overlap"></div>
+        </div>
     </main>
     <script src="/js/app.js"></script>
 </body>
