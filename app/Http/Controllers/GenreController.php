b67347642956e7576a060115a6fdef6df27f01d8
Lucas Charoing <charoing.lucas@gmail.com>
2022-12-03 17:09:08 +0100

    Add more explicit urls for genre pages

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -7,15 +7,16 @@ use Illuminate\Support\Facades\Http;
 
 class GenreController extends Controller
 {
-    public function show($genre_id, $page){
+    public function show($genre, $page){
+        $request = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=' . $_ENV['API_KEY'] . '&language=en-US');
+        $genre_id = collect($request->json()['genres'])->where('name', ucfirst($genre))->first()['id'];
         $request = Http::get('https://api.themoviedb.org/3/discover/movie?api_key=' . $_ENV['API_KEY'] . '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' . $page . '&with_genres=' . $genre_id . '&with_watch_monetization_types=flatrate');
         $movies = $request->json();
-        $request = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=' . $_ENV['API_KEY'] . '&language=en-US');
-        $genres = collect($request->json()['genres'])->where('id', $genre_id)->first()['name'];
         return view('genre', [
             'movies' => $movies,
-            'genre' => $genres,
-            'page' => $page
+            'genre' => $genre,
+            'page' => $page,
+            'total_pages' => $movies['total_pages'] < 500 ? $movies['total_pages']:500,
         ]);
     }
 }
