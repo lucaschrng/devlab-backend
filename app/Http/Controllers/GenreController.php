b65bba82b8199eba60c6d1cf4004f3bfc97a5976
Lucas Charoing <charoing.lucas@gmail.com>
2023-01-04 14:39:02 +0100

    Fix broken urls for genres with multiple words

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -20,13 +20,19 @@ class GenreController extends Controller
                 $filter_parameter = 'vote_average.desc';
                 break;
         }
+//        @dd(strtolower(str_replace('-', ' ', $genre)));
         $request = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=' . $_ENV['API_KEY'] . '&language=en-US');
-        $genre_id = collect($request->json()['genres'])->where('name', ucfirst($genre))->first()['id'];
-        $request = Http::get('https://api.themoviedb.org/3/discover/movie?api_key=' . $_ENV['API_KEY'] . '&language=en-US&sort_by=' . $filter_parameter . '&include_adult=false&include_video=false&page=' . $page . '&with_genres=' . $genre_id . '&with_watch_monetization_types=flatrate');
+//        @dd(collect($request->json()['genres'])->filter(function ($item) {
+//            return strtolower($item['name']) == 'tv movie';
+//        }));
+        $genre = collect($request->json()['genres'])->filter(function ($item) use ($genre) {
+            return strtolower($item['name']) == strtolower(str_replace('-', ' ', $genre));
+        })->first();
+        $request = Http::get('https://api.themoviedb.org/3/discover/movie?api_key=' . $_ENV['API_KEY'] . '&language=en-US&sort_by=' . $filter_parameter . '&include_adult=false&include_video=false&page=' . $page . '&with_genres=' . $genre['id'] . '&with_watch_monetization_types=flatrate');
         $movies = $request->json();
         return view('genre', [
             'movies' => $movies,
-            'genre' => $genre,
+            'genre' => $genre['name'],
             'filter' => $filter,
             'page' => $page,
             'total_pages' => min($movies['total_pages'], 500)
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <h2 class="px-24 text-3xl font-medium">Discover by genre</h2>
             <div class="grid grid-cols-3 gap-2 px-24 pt-10">
                 @foreach($genres as $genre)
-                    <a href="/genre/{{ strtolower($genre['name']) }}/popularity/1" class="text-center bg-lighter-bg text-2xl font-semibold p-6 rounded">{{ $genre['name'] }}</a>
+                    <a href="/genre/{{ str_replace(' ', '-', strtolower($genre['name'])) }}/popularity/1" class="text-center bg-lighter-bg text-2xl font-semibold p-6 rounded">{{ $genre['name'] }}</a>
                 @endforeach
             </div>
         </div>
