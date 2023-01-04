2cc1157e3828b8a67aa97467a7c3a65d56bf1c88
Lucas Charoing <charoing.lucas@gmail.com>
2023-01-04 15:35:50 +0100

    Add dashboard interface

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,19 @@
 <x-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                {{ csrf_field() }}
-                <button>Log Out</button>
-            </form>
+    <main class="p-[96px]">
+        <div class="flex items-center gap-6">
+            <h2 class="text-4xl font-medium">Hi, {{ Auth::user()->firstName }} {{ Auth::user()->lastName }} !</h2>
+            <ion-icon name="mail" class="text-3xl opacity-50 hover:opacity-100 hover:cursor-pointer"></ion-icon>
         </div>
-    </div>
+        <h3 class="text-xl text-white/60">{{ '@' . Auth::user()->username }}</h3>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+            {{ csrf_field() }}
+            <button class="mt-4 flex gap-2 items-center text-xl opacity-50 bg-lighter-bg p-2 rounded hover:opacity-100">Log Out<ion-icon name="log-out-outline" class="text-3xl"></ion-icon></button>
+        </form>
+        <div class="mt-24">
+            <h2 class="text-3xl font-medium">Your albums:</h2>
+            <div>
+
+            </div>
+        </div>
+    </main>
 </x-layout>
