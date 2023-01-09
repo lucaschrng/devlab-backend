2e121ee6699950a30889bd3ddff6f89bac6fc623
Lucas Charoing <charoing.lucas@gmail.com>
2023-01-09 18:17:25 +0100

    Fix migration table name

--- database/migrations/2022_11_29_150336_create_albums_likes_table.php
+++ database/migrations/2022_11_29_150336_create_albums_likes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('albums_like', function (Blueprint $table) {
+        Schema::create('albums_likes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('album_id')->constrained('albums');
             $table->foreignId('user_id')->constrained('users');
